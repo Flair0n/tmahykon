ea503a669142d8c1b5fc0dfa22f56e24a5b5536b
Emil Joshy <em1l.j03hy@gmail.com>
2025-09-24 15:43:47 +0530

    vercel analytics update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import PaymentSuccess from './PaymentSuccess.jsx';
@@ -8,9 +8,19 @@ import Home from './Home.jsx';
 import HowItWorks from './components/HowItWorks.jsx';
 import AdminDashboard from './components/AdminDashboard.jsx';
 
+function VercelAnalyticsListener() {
+  const location = useLocation();
+  useEffect(() => {
+    // Call Vercel Web Analytics on each SPA navigation
+    window.va?.('pageview');
+  }, [location.pathname, location.search, location.hash]);
+  return null;
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <VercelAnalyticsListener />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/form" element={<App />} />
